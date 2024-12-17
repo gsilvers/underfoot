389f7bbb7f1f2d1f024b42b611ef9b6930ff1847
Greg Silverstein <greg.silverstein@gmail.com>
2024-12-16 22:49:00 -0500

    chore: more learning from the rva guide

--- src/main.lua
+++ src/main.lua
@@ -1,39 +1,47 @@
--- function love.draw()
---    love.graphics.print("Hello World", 400, 300)
---    love.graphics.polygon('fill', 50, 0, 0, 100, 100, 100)
--- end
 
 local current_color = {1, 1, 1, 1}
+local seconds = 0
+
+local key_map = {
+  b = function()
+    current_color = {0, 0, 1, 1} -- Blue
+  end,
+  g = function()
+    current_color = {0, 1, 0, 1} -- Green
+  end,
+  r = function()
+    current_color = {1, 0, 0, 1} -- Red
+  end,
+  w = function()
+    current_color = {1, 1, 1, 1} -- White
+  end,
+  escape = function()
+    love.event.quit()
+  end
+}
 
 love.draw = function()
   local square = {100, 100, 100, 200, 200, 200, 200, 100}
 
+  -- Print a counter clock
+  local clock_display = 'Seconds: ' .. math.floor(seconds)
+  love.graphics.print(clock_display, 0, 0, 0, 2, 2)
+
   -- Initialize the square with the default color (white)
   love.graphics.setColor(current_color)
-  -- Draw the square
   love.graphics.polygon('fill', square)
 end
 
 
 love.keypressed = function(pressed_key)
-  if pressed_key == 'b' then
-    -- Blue
-    current_color = {0, 0, 1, 1}
-  elseif pressed_key == 'g' then
-    -- Green
-    current_color = {0, 1, 0, 1}
-  elseif pressed_key == 'r' then
-    -- Red
-    current_color = {1, 0, 0, 1}
-  elseif pressed_key == 'w' then
-    -- White
-    current_color = {1, 1, 1, 1}
-  elseif pressed_key == 'p' then
-     -- Purple
-     current_color = {1, 0, 1, 1}
-  elseif pressed_key == 'x' then
-     -- random shit
-     current_color = {.2, .3, .4, .85}
+  -- Check in the key map if there is a function
+  -- that matches this pressed key's name
+  if key_map[pressed_key] then
+    key_map[pressed_key]()
   end
+end
 
+love.update = function(dt)
+  -- Add up all the delta time as we get it
+  seconds = seconds + dt
 end
