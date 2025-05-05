7ee56b971ec57bf77537d37ccea0f52bd8d74767
Greg Silverstein <greg.silverstein@gmail.com>
2025-05-04 22:53:37 -0400

    add items

---
+++ conf.lua
@@ -0,0 +1,10 @@
+love.conf = function(t)
+   t.gammacorrect = true
+   t.title, t.identity = "Underfoot", "Minimal"
+   t.modules.joystick = false
+   t.modules.physics = false
+   t.window.width = 1920
+   t.window.height = 1080
+   t.window.vsync = false
+   t.version = "11.5"
+end
